00ba41232e7199f5c9e6a1562030b900e4dd89aa
maxprogit <sidewiki@live.com.mx>
2023-02-15 09:58:44 -0600

    corregir validaciones

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const bcrypt = require ('bcrypt')
 const { initializeApp } = require ('firebase/app')
-const { getFirestore } = require ('firebase/firestore')
+const { getFirestore, setDoc, getDoc, collection } = require ('firebase/firestore')
 const dotEnv = require('dotenv')    
 
 //Configuracion de firebase
@@ -21,6 +21,60 @@ const firebaseConfig = {
 
   //Inicializar el servidor
   const app = express()
+
+  app.use(express.json())
+
+  //Rutas para las peticiones EndPoint | API
+  //Ruta para el registro
+  app.post('/registro', (req, res) =>{
+    const { name, lastname, email, password, number} = req.body
+
+    //Validacion de los datos 
+    if(name.length < 3) {
+      res.json({
+        'alert': 'nombre requiere minimo 3 caracters'
+      })
+    } else if (!email.length){
+      res.json({
+        'alert': 'Debes escribir un correo electronico'
+      })
+    }else if (password.length) {
+      res.json({
+        'alert': 'nombre requiere minimo 8 caracteres'
+      })
+    } else if (!Number(number) || number.length < 10) {
+      res.json( {
+        'alert': 'Introduce un numero telefonico correcto'
+      })
+    } else if (lastname.length) {
+      res.json({
+        'alert': ''
+    })
+         //Verificar que el correo no exista en la coleccion
+         getDoc(doc(user, email)).then((user => {
+          if (user.exists()) {
+
+          }
+         })
+      
+    }
+    })
+    //Guardar en la BD 
+    getDoc(doc(user, email), req.body).then(() => {
+      res.json({
+        
+      
+    )
+    
+  app.get('/usuarios', (req, res) => {
+    const users = collection(db 'users')
+    console.log('usuarios', users)
+    res.json({
+      'alert': 'success',
+      data
+    })
+  })
+
 
   const PORT = process.env.PORT || 19000
 
